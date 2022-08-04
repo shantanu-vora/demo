951aeedd3926f6d47a5146c7874876fe60af5aba
ssvora <ssvora@C02F96WEMD6M.local>
2022-08-04 19:24:50 +0530

    Add a GeneralInterceptor

---
+++ src/main/java/com/shantanu/demo/config/InterceptorConfiguration.java
@@ -0,0 +1,20 @@
+package com.shantanu.demo.config;
+
+import com.shantanu.demo.interceptor.GeneralInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+	@Autowired
+	private GeneralInterceptor generalInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(generalInterceptor);
+	}
+
+
+}
